e62f3843b8230bd7f17502c7f540f91d4bf8c592
Phil Chen <06fahchen@gmail.com>
2018-08-15 17:29:40 +0800

    Add metadata

--- mix.exs
+++ mix.exs
@@ -12,7 +12,9 @@ defmodule AlixirSms.MixProject do
       homepage_url: @project_host,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      package: package(),
     ]
   end
 
@@ -22,10 +24,24 @@ defmodule AlixirSms.MixProject do
     ]
   end
 
+  defp description do
+    "Aliyun SMS adapter."
+  end
+
   defp deps do
     [
       {:alixir_core, "~> 0.1.0"},
       {:poison, "~> 3.1"},
+    ]
+  end
+
+  defp package do
+    [
+      name: :alixir_core,
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["fahchen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @project_host}
     ]
   end
 end
